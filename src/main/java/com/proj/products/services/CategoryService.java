7a80ab15ddf3e8fdc032e9a21634a91145590be4
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-02-10 18:09:44 -0300

    Implement the create functionality of the crud

--- src/main/java/com/proj/products/services/CategoryService.java
+++ src/main/java/com/proj/products/services/CategoryService.java
@@ -30,4 +30,12 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found " + id));
 		return new CategoryDTO(entity);
 	}
+
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());;
+		entity = repository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 }
