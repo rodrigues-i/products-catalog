f9ec6126d03c71d11a80c9a80f0db7eed54cc695
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-02-10 18:32:51 -0300

    Implement the update functionality of the crud

--- src/main/java/com/proj/products/services/CategoryService.java
+++ src/main/java/com/proj/products/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.proj.products.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,25 +18,40 @@ import com.proj.products.services.exceptions.ResourceNotFoundException;
 public class CategoryService {
 	@Autowired
 	CategoryRepository repository;
-	
-	@Transactional
+
+	@Transactional(readOnly = true)
 	public Page<CategoryDTO> findAllPaged(Pageable pageable) {
 		Page<Category> list = repository.findAll(pageable);
 		return list.map(cat -> new CategoryDTO(cat));
 	}
-	
-	@Transactional
+
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found " + id));
 		return new CategoryDTO(entity);
 	}
 
+	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
-		entity.setName(dto.getName());;
+		entity.setName(dto.getName());
+		;
 		entity = repository.save(entity);
-		
+
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			;
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Entity not found " + id);
+		}
 	}
 }
