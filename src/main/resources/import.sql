5e17ad81bd6ba3986a67e3078188b514b3798db7
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-02-12 12:58:45 -0300

    Add database seed for the product_tb table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,30 @@
 INSERT INTO category_tb (name, created_At) VALUES ('Electronics', NOW());
 INSERT INTO category_tb (name, created_At) VALUES ('Fashion', NOW());
-INSERT INTO category_tb (name, created_At) VALUES ('Home and Garden', NOW());
+INSERT INTO category_tb (name, created_At) VALUES ('Gardening', NOW());
 INSERT INTO category_tb (name, created_At) VALUES ('Sports and Outdoors', NOW());
-INSERT INTO category_tb (name, created_At) VALUES ('Beauty and Personal Care', NOW());
 INSERT INTO category_tb (name, created_At) VALUES ('Books and Media', NOW());
 INSERT INTO category_tb (name, created_At) VALUES ('Food and Beverages', NOW());
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Apple iPhone 12', 'Latest iPhone with 6.1-inch display', 799.00, 'https://example.com/iphone12.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Samsung Galaxy S21', 'Latest Samsung smartphone with 5G capabilities', 999.00, 'https://example.com/samsungs21.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Microsoft Surface Laptop 4', 'Lightweight and powerful laptop from Microsoft', 899.00, 'https://example.com/surfacelaptop4.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Bose QuietComfort 35 II Wireless Headphones', 'Noise-cancelling headphones with Alexa voice control', 329.00, 'https://example.com/boseheadphones.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Canon EOS Rebel T7 DSLR Camera', 'Affordable entry-level DSLR camera with 24.1 megapixel CMOS sensor', 449.00, 'https://example.com/canont7.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Adidas Ultraboost 20 Running Shoes', 'Running shoes with responsive Boost cushioning', 179.00, 'https://example.com/ultraboost20.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Nike Air Zoom Pegasus 38 Running Shoes', 'Comfortable and lightweight running shoes from Nike', 129.00, 'https://example.com/airzoompegasus38.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Under Armour HOVR Phantom SE Running Shoes', 'Running shoes with energy-returning HOVR technology', 139.00, 'https://example.com/hovrphantomse.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Columbia Bugaboo II Fleece Interchangeable Headwear', 'Interchangeable headwear for all-day comfort and warmth', 29.00, 'https://example.com/bugaboofleece.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('North Face ThermoBall Eco Insulated Jacket', 'Insulated jacket made with recycled materials', 199.00, 'https://example.com/thermoball.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Patagonia Nano Puff Insulated Jacket', 'Lightweight and packable insulated jacket', 199.00, 'https://example.com/nanopuff.jpeg'); 
+
+INSERT INTO product_tb (name, description, price, img_Url) VALUES ('Black+Decker 20V MAX Cordless Drill', 'Cordless drill with 20V MAX lithium ion battery', 99.00, NULL);
